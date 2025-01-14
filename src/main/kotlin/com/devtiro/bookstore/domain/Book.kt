c1afb7b9eaf892f9f8c0ac78abd141342020a8c0
Seyyed Mahdi Hassanpour <seyyed.hassanpour@traderepublic.com>
2025-01-14 11:58:38 +0100

    init

---
+++ src/main/kotlin/com/devtiro/bookstore/domain/Book.kt
@@ -0,0 +1,5 @@
+package com.devtiro.bookstore.domain
+
+class Book (val isbn:String,
+            val title:String, val author:Author, val description:String,
+             val price:Double, val image:String)
